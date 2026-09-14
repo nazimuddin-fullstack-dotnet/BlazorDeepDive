9561bbcca599cff7db77560f9645e99f444e38b2
Nazim Nakib <Nazim.Nakib@hirdaramani.com>
2026-09-14 09:18:49 +0600

    Implemented Edit functionality

---
+++ BlazorDeepDive/ServerManagement/Components/Models/CitiesRepository.cs
@@ -0,0 +1,17 @@
+﻿namespace ServerManagement.Components.Models
+{
+    public static class CitiesRepository
+    {
+        private static List<string> cities = new List<string>()
+        {
+            "Toronto",
+            "Montreal",
+            "Ottawa",
+            "Calgary",
+            "Halifax"
+        };
+
+        public static List<string> GetCities() => cities;
+
+    }
+}
